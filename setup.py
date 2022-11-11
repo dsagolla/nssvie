08338cf07c58fc5a3f84b5ef7f25504282547775
dsagolla <daniel.sagolla@mailbox.org>
2022-11-11 13:28:21 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -63,7 +63,7 @@ setuptools.setup(
         'docs': docs_requirements,
     },
     classifiers=[
-        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.9',
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
         'Operating System :: OS Independent',
         'Natural Language :: English',
