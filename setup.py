19d4f435b870df1a9fa3b9c1f82f5152638d8f61
dsagolla <daniel.sagolla@mailbox.org>
2022-11-22 16:28:47 +0100

    Update setup.py
    Change classifierts
    - remove Python 3.6
    - add Python 3.11

--- setup.py
+++ setup.py
@@ -61,11 +61,11 @@ setuptools.setup(
         'docs': docs_requirements,
     },
     classifiers=[
-        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
         'Operating System :: OS Independent',
         'Natural Language :: English',
