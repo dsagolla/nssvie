b0a5a47101cf4fa5dd6f708ef847f1f84256cf3f
dsagolla <daniel.sagolla@mailbox.org>
2022-11-23 00:55:33 +0100

    Update setup.py
    New version

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ tracker_url = url + "/issues"
 # Setup
 setuptools.setup(
     name="nssvie",
-    version="0.0.3",
+    version="0.0.4",
     author="Daniel Sagolla",
     author_email="daniel.sagolla@udo.edu",
     description=r"Numerical solution of stochastic Volterra integral " +
