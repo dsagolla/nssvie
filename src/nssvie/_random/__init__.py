6d3cce432c9744d43de32281a9952f1dcb08d7b4
dsagolla <dsagolla@mailbox.org>
2022-12-12 21:01:09 +0100

    Create __init__.py

---
+++ src/nssvie/_random/__init__.py
@@ -0,0 +1,6 @@
+"""Random Module.
+"""
+# from nssvie._random.gaussian import GaussianProcess
+# from nssvie._random.brownian_motion import BrownianMotion
+
+# __all__ = ['GaussianProcess', 'BrownianMotion']
