313ac1f9df902fa95299aaea9c86117498c7d473
dsagolla <dsagolla@mailbox.org>
2022-12-21 22:22:04 +0100

    Update svie.py

--- src/nssvie/_integral_equations/svie.py
+++ src/nssvie/_integral_equations/svie.py
@@ -4,15 +4,14 @@ Classes
 -------
 SVIE
 """
-from numpy import (
-    eye,
-    transpose
-)
+import numpy as np
 
 from scipy.linalg import solve_triangular
 
 from nssvie._orthogonal_functions import BlockPulseFunctions
 from nssvie._integral_equations.base import IntegralEquation
+
+from typing import Callable
 
 
 class SVIE(IntegralEquation):
@@ -25,46 +24,62 @@ class SVIE(IntegralEquation):
         X_t = f(t) + \\int\\limits_0^t k_1(s,t) X_s \\ ds
         + \\int\\limits_0^t k_2(s,t) X_s \\ dB_s \\qquad t \\in [0,T),
 
-    where :math:`X_t` is an unknown stochastic process, :math:`B_t` the
-    Brownian motion,
+    where
 
-    .. math::
+    + :math:`X_t` is an unknown stochastic process,
+    + :math:`B_t` the Brownian motion,
+    + the last integral in :eq:`svie_doc`
+
+      .. math::
 
         \\int\\limits_0^t k_2(s,t) X_s \\ dB_s
 
-    is the Itô-integral and :math:`f \\in L^2([0,T))` and
-    :math:`k_1, \\ k_2 \\in L^2([0,T) \\times [0,T))`.
+      is the Itô-integral and
+    + :math:`f \\in L^2([0,T))` and
+      :math:`k_1, \\ k_2 \\in L^2([0,T) \\times [0,T))` are continuous
+      and square integrable functions.
 
     Parameters
     ----------
-    f : callable
+    f : Callable[[float], float]
         Function :math:`f` in :eq:`svie_doc`.
-    kernel_1, kernel_2 : callable
+    kernel_1, kernel_2 : Callable[[float, float], float]
         The kernels :math:`k_1` and :math:`k_2` in :eq:`svie_doc`.
-    T : float, default 1.0
-        The right hand side of the interval :math:`[0,T)`.
+    T : float, optional
+        The right hand side of the interval :math:`[0,T)`, by default
+        1.0.
 
     Methods
     -------
     solve_numerical
     """
 
-    def __init__(self, f, kernel_1, kernel_2, T=1.0):
+    def __init__(
+        self,
+        f: Callable[[float], float],
+        kernel_1: Callable[[float, float], float],
+        kernel_2: Callable[[float, float], float],
+        T: float = 1.0
+    ) -> None:
         self.f = f
         self.kernel_1 = kernel_1
         self.kernel_2 = kernel_2
         super().__init__(T=T)
 
-    def solve_numerical(self, m=20, solve_method='bpf'):
+    def solve_numerical(
+        self,
+        m: int,
+        solve_method: str = 'bpf'
+    ) -> np.array:
         """
         Compute a numerical solution for the given stochastic Volterra
         integral equation.
 
         Parameters
         ----------
-        m : int, default 20
+        m : int
             The number of equidistant intervals to divide :math:`[0,T)`.
-        solve_method : str, default 'bpf'
+        solve_method : str, optional
             If ``solve_methods='bpf'`` an algorithm presented in
             `Maleknejad et. al (2012)
             <https://www.sciencedirect.com/science/
@@ -72,7 +87,8 @@ class SVIE(IntegralEquation):
             operational matrix of integration based on block pulse
             functions. For the solution of :math:`MX=F`, where :math:`M`
             is a triangular matrix
-            :func:`scipy.linalg.solve_triangular` is used.
+            :func:`scipy.linalg.solve_triangular` is used, by default
+            ``bpf``.
 
         Returns
         -------
@@ -85,8 +101,8 @@ class SVIE(IntegralEquation):
             # based on block pulse functions as suggested in
             # Maleknejad et. al (2012).
             bpf = BlockPulseFunctions(T=self.T, m=m)
-            M = transpose(
-                eye(m)
+            M = np.transpose(
+                np.eye(m)
                 - bpf._matrix_b1(kernel_1=self.kernel_1)
                 - bpf._matrix_b2(kernel_2=self.kernel_2)
             )
